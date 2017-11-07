43592af9b2d5ceb632388cb5be7b2b80f4e09893
YGL <2369015621@qq.com>
2017-11-07 13:34:56 +0800

    start

---
+++ app/src/main/java/com/example/TestReader/ui/reader/BookPageFactory.java
@@ -0,0 +1,399 @@
+package com.example.TestReader.ui.reader;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Typeface;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.TestReader.R;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by YGL on 2017/10/12.
+ */
+//用于生产给PageWidget显示的Bitmap
+public class BookPageFactory {
+    private static final String TAG = "ReaderActivity";
+    private Context mContext;
+
+    private int screenWidth;
+    private int screenHeight;
+
+    private Paint p;
+    private Paint percentPaint;
+    private Paint coverPaint;
+
+    private String book;
+    private List<String> pageString;
+
+    private int textColor;
+    private int textSize;
+    private Typeface textTypeface;
+
+    private int marginWidth;
+    private int marginLine;
+    private int maxLine;
+    private int lineWidth;
+    private int percentSize;
+
+    private int backColor;
+
+    private int readAddress;
+
+    private DecimalFormat df;
+
+    private Bitmap coverImage;
+    private String title;
+    private String subTitle;
+
+    public BookPageFactory(Context context,int screenWidth, int screenHeight){
+        this.mContext=context;
+        this.screenWidth=screenWidth;
+        this.screenHeight=screenHeight;
+        book="";
+        readAddress=0;
+
+        //读取默认配置
+        marginWidth=(int) mContext.getResources().getDimension(R.dimen.width_margin);
+        marginLine=(int) mContext.getResources().getDimension(R.dimen.line_margin);
+        backColor = mContext.getResources().getColor(R.color.reader_back);
+        textColor=mContext.getResources().getColor(R.color.reader_body);
+        percentSize=(int)mContext.getResources().getDimension(R.dimen.percent_size);
+        //设置画笔
+        p=new Paint();
+        p.setColor(textColor);
+        textSize=(int) mContext.getResources().getDimension(R.dimen.reading_body_size);
+        p.setTextSize(textSize);
+        textTypeface=Typeface.createFromAsset(mContext.getAssets(), "9t.ttf");
+        p.setTypeface(textTypeface);
+        coverPaint=new Paint();
+        percentPaint=new Paint();
+        percentPaint.setTextSize(percentSize);
+        percentPaint.setColor(textColor);
+        //计算屏幕可以显示文字的行数
+        maxLine=screenHeight/(marginLine+textSize)-1;//减去的一行用于显示阅读进度
+        //计算一行的长度
+        lineWidth=screenWidth-marginWidth*2;
+
+        df = new DecimalFormat("#0.0");
+    }
+
+    public void onDraw(Canvas c) {
+        c.drawColor(backColor);
+
+        if(readAddress<0){
+            //画封面
+            //设置封面专用画笔
+            coverPaint.setColor(mContext.getResources().getColor(R.color.Black));
+            int titleSize=textSize*6/5;
+            coverPaint.setTextSize(titleSize);
+            coverPaint.setTypeface(textTypeface);
+            int marginTop=(screenHeight-(screenWidth+titleSize*2+textSize*2+marginLine*4))/2;
+            //画封面图片
+            if(coverImage==null){
+                //设置默认图片
+                coverImage=BitmapFactory.decodeResource(mContext.getResources(),R.drawable.empty_detail);
+            }
+            int iWidth=coverImage.getWidth();
+            int iHeight=coverImage.getHeight();
+            Rect r=new Rect();
+            Rect rr=new Rect();
+            if(iWidth>=iHeight){
+                //横向图片
+                r.set((iWidth-iHeight)/2,0,iHeight+(iWidth-iHeight)/2,iHeight);
+                rr.set(0,marginTop,screenWidth,screenWidth+marginTop);
+            }else {
+                //纵向图片
+                if((iHeight/iWidth)>(screenHeight/screenWidth)){
+                    int ih=iWidth*screenHeight/screenWidth;
+                    r.set(0,(iHeight-ih)/2,iWidth,ih+(iHeight-ih)/2);
+                    rr.set(0,0,screenWidth,screenHeight);
+                }else {
+                    int iw=iHeight*screenWidth/screenHeight;
+                    r.set((iWidth-iw)/2,0,iw+(iWidth-iw)/2,iHeight);
+                    rr.set(0,0,screenWidth,screenHeight);
+                }
+            }
+            c.drawBitmap(coverImage,r,rr,coverPaint);
+            //画标题
+            Paint wordBackground =new Paint();
+            wordBackground.setColor(mContext.getResources().getColor(R.color.Gray));
+            Rect titleRect=new Rect();
+            int lineX=marginWidth;
+            int lineY=marginTop+screenWidth;
+            if(!TextUtils.isEmpty(title)){
+                List<String> list=getLineString(coverPaint,title);
+                for (String line: list){
+                    lineY+=(titleSize+marginLine);
+                    titleRect.set(0,lineY-titleSize,screenWidth,lineY+marginLine);
+                    c.drawRect(titleRect,wordBackground);
+                    c.drawText(line,lineX,lineY,coverPaint);
+                }
+            }
+            coverPaint.setColor(mContext.getResources().getColor(R.color.MinBlack));
+            coverPaint.setTextSize(textSize*4/5);
+            //画副标题
+            if(!TextUtils.isEmpty(subTitle)){
+                List<String> list=getLineString(coverPaint,subTitle);
+                for (String line: list){
+                    lineY+=(textSize+marginLine);
+                    titleRect.set(0,lineY-textSize,screenWidth,lineY+marginLine);
+                    c.drawRect(titleRect,wordBackground);
+                    c.drawText(line,lineX,lineY,coverPaint);
+                }
+            }
+        }else {
+            //画文字
+            pageString=getPageString(readAddress);
+            int lineX=marginWidth;
+            int lineY=0;
+            for (String line: pageString){
+                lineY+=marginLine+textSize;
+                c.drawText(line,lineX,lineY,p);
+            }
+
+            //画进度
+            float fPercent = (float) (readAddress * 1.0 / book.length());
+            String strPercent = df.format(fPercent * 100) + "%";
+            lineY+=marginLine+textSize;
+            c.drawText(strPercent,marginWidth,lineY, percentPaint);
+        }
+    }
+
+    //读取指定位置一页内容
+    private List<String> getPageString(int address){
+        List<String> list=new ArrayList<>();
+        if(!TextUtils.isEmpty(book)&&address>=0){
+            for (int i=0;i<maxLine;i++){
+                String word=book.substring(address,
+                        (address+oneLineCharacterQuantity(p)*2)>book.length()?book.length():(address+oneLineCharacterQuantity(p)*2));
+                int a=p.breakText(word,true,lineWidth,null);//自动折行
+                int b=word.indexOf("\n");
+                if(b!=-1&&b<=a){
+                    //如果有转行符
+                    b+=1;
+                }else {
+                    b=word.lastIndexOf(" ",a);
+                    if (b!=-1&&isMultipleString(word.substring(0,a))){
+                        //如果有空格
+                        b+=1;
+                    }else {
+                        //既没有转行符，也没有空格
+                        b=a;
+                    }
+                }
+                list.add(word.substring(0,b));
+                address+=b;
+            }
+        }
+        return list;
+    }
+    //将一段字符串分成多行
+    private List<String> getLineString(Paint paint,String string){
+        List<String> list=new ArrayList<>();
+        if(!TextUtils.isEmpty(string)){
+            int address=0;
+            while (address<string.length()){
+                string=string.substring(address,string.length());
+                int a=paint.breakText(string,true,lineWidth,null);//自动折行
+                int b=string.indexOf("\n");
+                if(b!=-1&&b<=a){
+                    //如果有转行符
+                    b+=1;
+                }else {
+                    b=string.lastIndexOf(" ",a);
+                    if (b!=-1&&isMultipleString(string.substring(0,a))&&a<string.length()){
+                        //如果有空格
+                        b+=1;
+                    }else {
+                        //既没有转行符，也没有空格
+                        b=a;
+                    }
+                }
+                list.add(string.substring(0,b));
+                address+=b;
+            }
+        }
+        return list;
+    }
+
+    //翻到上一页
+    public void prePage(){
+        if (readAddress<=-1){
+            showMessage(mContext.getResources().getString(R.string.first_page_toast));
+            return;
+        }
+        readAddress=prePageAddress(readAddress);
+    }
+
+    //翻到下一页
+    public void nextPage(){
+        if (readAddress>=book.length()){
+            showMessage(mContext.getResources().getString(R.string.last_page_toast));
+            return;
+        }
+        readAddress=nextPageAddress(readAddress);
+    }
+
+    //得到上一页大概的起始位置
+    private int prePageAddress(int address){
+        if(address<=0){
+            return -1;
+        }
+        int myAddress=address;
+        if (!TextUtils.isEmpty(book)){
+            //以下为风骚时刻，请勿轻易模仿
+            //使用穷举法求解
+            int z=nextPageAddress(myAddress);
+            int accuracy=(oneLineCharacterQuantity(p)*maxLine)/4;//设置开始精度
+            while (z!=address||z==book.length()){
+                if(z<address){
+                    myAddress+=accuracy;
+                    z=nextPageAddress(myAddress);
+                    if(z>address){//增加精度
+                        if (accuracy==1){
+                            break;
+                        }
+                        accuracy=accuracy/2;
+                    }
+                }else {
+                    myAddress-=accuracy;
+                    z=nextPageAddress(myAddress);
+                    if(z<address){//增加精度
+                        if (accuracy==1){
+                            myAddress+=1;
+                            break;
+                        }
+                        accuracy=accuracy/2;
+                    }
+                }
+                Log.i("performanceLog","myAddress:"+myAddress);
+            }
+            Log.i("performanceLog","最终myAddress="+myAddress);
+        }
+        if(myAddress<0){
+            return 0;
+        }else {
+            return myAddress;
+        }
+    }
+
+    //得到下一页的起始位置
+    private int nextPageAddress(int address){
+        if(address<0){
+            return 0;
+        }
+        if(!TextUtils.isEmpty(book)){
+            for (int i=0;i<maxLine;i++){
+                //计算每一行的长度
+                String word=book.substring(address,
+                        (address+oneLineCharacterQuantity(p)*2)>book.length()?book.length():(address+oneLineCharacterQuantity(p)*2));
+                int a=p.breakText(word,true,lineWidth,null);
+                int b=word.indexOf("\n");
+                if(b!=-1&&b<=a){
+                    //如果有转行符
+                    b=b+1;
+                }else {
+                    b=word.lastIndexOf(" ",a);
+                    if (b!=-1&&isMultipleString(word.substring(0,a))){
+                        //如果有空格
+                    }else {
+                        //既没有转行符，也没有空格
+                        b=a;
+                    }
+                }
+                //加上每一行的长度
+                address+=b;
+            }
+        }
+        return address;
+    }
+
+    //验证此行是否至少包涵两段字
+    private Boolean isMultipleString(String string){
+        Boolean firstString=false;
+        Boolean intermediateSpace=false;
+        Boolean secondString=false;
+        for (int i=0;i<string.length();i++){
+            if(!string.substring(i,i+1).equals(" ")&&!intermediateSpace&&!secondString){
+                firstString=true;
+            }
+            if (firstString&&string.substring(i,i+1).equals(" ")&&!secondString){
+                intermediateSpace=true;
+            }
+            if (firstString&&intermediateSpace&&!string.substring(i,i+1).equals(" ")){
+                secondString=true;
+            }
+        }
+        return firstString&&intermediateSpace&&secondString;
+    }
+
+    //计算一行大概能容纳的字符串数
+    private int oneLineCharacterQuantity(Paint paint){
+        String testString="aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+        return paint.breakText(testString,true,lineWidth,null);
+    }
+
+    private void showMessage(String message){
+        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
+    }
+
+    public int getReadAddress() {
+        return readAddress;
+    }
+    public int getBookLength(){return book.length();}
+
+    //以下为设置方法
+    public void setBook(String book){
+        setBook(book,-1);
+    }
+    public void setBook(String book,int readAddress){
+        setBook(book,readAddress,null,null,null);
+    }
+    public void setBook(String book,int readAddress,Bitmap coverImage,String title,String subTitle){
+        this.book=book;
+        this.readAddress=readAddress;
+        this.coverImage=coverImage;
+        this.title=title;
+        this.subTitle=subTitle;
+    }
+
+    /*
+    public void setMargin(int marginX){
+        this.marginWidth=marginX;
+    }
+
+    public void setBackColor(int backColor){
+        this.backColor=backColor;
+    }
+
+    public void setTextColor(int textColor) {
+        this.textColor = textColor;
+    }
+
+    public void setTextSize(int textSize) {
+        this.textSize = textSize;
+    }
+
+    public void setTextTypeface(Typeface textTypeface) {
+        this.textTypeface = textTypeface;
+    }
+    */
+
+    public void setReadAddress(int readAddress) {
+        this.readAddress = readAddress;
+    }
+
+    public void setCoverImage(Bitmap coverImage) {
+        this.coverImage = coverImage;
+    }
+}
---
+++ app/src/main/java/com/example/TestReader/ui/reader/ReaderActivity.java
@@ -0,0 +1,358 @@
+package com.example.TestReader.ui.reader;
+
+import android.app.LoaderManager;
+import android.content.Intent;
+import android.content.Loader;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.format.DateUtils;
+import android.util.Log;
+import android.view.Display;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
+import com.example.TestReader.R;
+import com.example.TestReader.data.ArticleLoader;
+import com.example.TestReader.data.ItemsContract;
+import com.example.TestReader.remote.Config;
+import com.example.TestReader.ui.ArticleListActivity;
+import com.example.TestReader.ui.ImageLoaderHelper;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/**
+ * Created by YGL on 2017/10/4.
+ */
+
+public class ReaderActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>,SeekBar.OnSeekBarChangeListener {
+    private static final String TAG = "ReaderActivity";
+    private PageView mPageView;
+    private int screenHeight;
+    private int readHeight; // 电子书显示高度
+    private int screenWidth;
+    private Bitmap mCurPageBitmap, mNextPageBitmap;
+    public static Canvas mCurPageCanvas, mNextPageCanvas;
+    private long mItemId;//文章ID
+    private Cursor mCursor;
+    private BookPageFactory bookPageFactory;
+    private FrameLayout control;
+    private SeekBar seekBar;
+    private FloatingActionButton share;
+    private GregorianCalendar START_OF_EPOCH = new GregorianCalendar(2,1,1);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setContentView(R.layout.activity_article_content);
+        hideSystemUI();
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);//保持屏幕常亮
+
+        String title="";
+        if (savedInstanceState == null) {//为什么要判断这个
+            if (getIntent() != null && getIntent().getData() != null) {
+                mItemId = ItemsContract.Items.getItemId(getIntent().getData());
+                title=getIntent().getStringExtra(ArticleListActivity.TITLE);
+            }
+        }
+
+        getLoaderManager().initLoader(0, null, this);
+
+        //获取屏幕宽高
+        WindowManager manage = getWindowManager();
+        Display display = manage.getDefaultDisplay();
+        Point displaysize = new Point();
+        display.getSize(displaysize);
+        screenWidth = displaysize.x;
+        screenHeight = displaysize.y;
+        readHeight = screenHeight - screenWidth / 320;
+
+        mCurPageBitmap = Bitmap.createBitmap(screenWidth, screenHeight, Bitmap.Config.RGB_565);      //android:LargeHeap=true  use in  manifest application
+        mNextPageBitmap = Bitmap.createBitmap(screenWidth, screenHeight, Bitmap.Config.RGB_565);
+        mCurPageCanvas = new Canvas(mCurPageBitmap);
+        mNextPageCanvas = new Canvas(mNextPageBitmap);
+
+        mPageView = new PageView(this, screenWidth, readHeight);
+        FrameLayout contentLayout=(FrameLayout) findViewById(R.id.content_layout);
+        contentLayout.addView(mPageView);
+        Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar=getSupportActionBar();
+        if(actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle(title);
+        }
+        seekBar=(SeekBar)findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(this);
+        control=(FrameLayout)findViewById(R.id.control);
+        control.setVisibility(View.GONE);
+        share =(FloatingActionButton)findViewById(R.id.share);
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, Config.APP_LOCATION);
+                Intent chooserIntent=Intent.createChooser(intent,getResources().getString(R.string.share_title));
+                if (chooserIntent == null) {
+                    return;
+                }
+                try {
+                    startActivity(chooserIntent);
+                } catch (android.content.ActivityNotFoundException ex) {
+                    Toast.makeText(getBaseContext(), "Can't find share component to share", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        bookPageFactory=new BookPageFactory(getBaseContext(),screenWidth,screenHeight);
+        bookPageFactory.onDraw(mCurPageCanvas);
+        //设置翻页过场动画
+        mPageView.setBitmaps(mCurPageBitmap, mNextPageBitmap);
+        mPageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent e) {
+                boolean ret = false;
+                if (v == mPageView) {
+                    if (e.getAction() == MotionEvent.ACTION_DOWN) {
+                        mPageView.abortAnimation();
+                        mPageView.calcCornerXY(e.getX(), e.getY());
+                        bookPageFactory.onDraw(mCurPageCanvas);
+
+                        int x = (int) e.getX();
+                        int y = (int) e.getY();
+                        if(showOrHideControl(x,y)){//控制控制菜单
+                            return false;
+                        }
+                        if (x < screenWidth / 2) {// 向前翻
+                            bookPageFactory.prePage();
+                        } else if (x >= screenWidth / 2) {// 向后翻
+                            bookPageFactory.nextPage();
+                        }
+                        bookPageFactory.onDraw(mNextPageCanvas);
+                        //设置翻页过场动画
+                        mPageView.setBitmaps(mCurPageBitmap, mNextPageBitmap);
+                    }
+                    //输入触控坐标，开始绘制过场动画
+                    ret = mPageView.doTouchEvent(e);
+                    return ret;
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return ArticleLoader.newInstanceForItemId(getBaseContext(), mItemId);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+        mCursor = cursor;
+        if (mCursor != null && !mCursor.moveToFirst()) {
+            Log.e(TAG, "Error reading item detail cursor");
+            mCursor.close();
+            mCursor = null;
+        }
+        if(mCursor!=null){
+            String publishedDate=getPublishedDate(mCursor);
+            bookPageFactory.setBook(
+                    mCursor.getString(ArticleLoader.Query.BODY),
+                    getReadAddress(mCursor.getString(ArticleLoader.Query.TITLE)),
+                    null,
+                    mCursor.getString(ArticleLoader.Query.TITLE),
+                    publishedDate);
+            ImageLoaderHelper.getInstance(getBaseContext()).getImageLoader()
+                    .get(mCursor.getString(ArticleLoader.Query.PHOTO_URL), new ImageLoader.ImageListener() {
+                        @Override
+                        public void onResponse(ImageLoader.ImageContainer imageContainer, boolean b) {
+                            Bitmap bitmap = imageContainer.getBitmap();
+                            if (bitmap != null) {
+                                bookPageFactory.setCoverImage(bitmap);
+                            }
+                        }
+
+                        @Override
+                        public void onErrorResponse(VolleyError volleyError) {
+
+                        }
+                    });
+            bookPageFactory.onDraw(mCurPageCanvas);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> cursorLoader) {
+        mCursor = null;
+    }
+
+    /**
+     * 隐藏菜单。沉浸式阅读
+     */
+    private void hideSystemUI() {
+        // Set the IMMERSIVE flag.
+        // Set the content to appear under the system bars so that the content
+        // doesn't resize when the system bars hide and show.
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        //  | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
+    }
+    private void showSystemUI() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
+    }
+
+    private Boolean showOrHideControl(int x,int y){
+        if(control.getVisibility()==View.GONE){
+            if((x>screenWidth/3&&x<screenWidth*2/3)&&(y>screenHeight/3&&y<screenHeight*2/3)){
+                showSystemUI();
+                control.setVisibility(View.VISIBLE);
+                seekBar.setProgress((int)((bookPageFactory.getReadAddress()*100.0)/bookPageFactory.getBookLength()));
+            }else {
+                return false;
+            }
+        }else {
+            hideSystemUI();
+            control.setVisibility(View.GONE);
+        }
+        return true;
+    }
+
+    private String getPublishedDate(Cursor cursor){
+        Date publishedDate = parsePublishedDate(cursor);
+        if (!publishedDate.before(START_OF_EPOCH.getTime())) {
+            return DateUtils.getRelativeTimeSpanString(publishedDate.getTime(), System.currentTimeMillis(), DateUtils.HOUR_IN_MILLIS, DateUtils.FORMAT_ABBREV_ALL).toString()
+                    + " by "
+                    + mCursor.getString(ArticleLoader.Query.AUTHOR);
+        } else {
+            // If date is before 1902, just show the string
+            return new SimpleDateFormat().format(publishedDate)
+                    + " by "
+                    + mCursor.getString(ArticleLoader.Query.AUTHOR);
+        }
+    }
+    private Date parsePublishedDate(Cursor c) {
+        try {
+            String date = c.getString(ArticleLoader.Query.PUBLISHED_DATE);
+            return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.sss").parse(date);
+        } catch (ParseException ex) {
+            Log.e(TAG, ex.getMessage());
+            Log.i(TAG, "passing today's date");
+            return new Date();
+        }
+    }
+
+    private int getReadAddress(String title){
+        SharedPreferences sharedPreferences=getSharedPreferences("ReadAddress",MODE_PRIVATE);
+        return sharedPreferences.getInt(title,-1);
+    }
+    private void setReadAddress(String title,int readAddress){
+        SharedPreferences.Editor editor=getSharedPreferences("ReadAddress",MODE_PRIVATE).edit();
+        editor.putInt(title,readAddress);
+        editor.commit();
+    }
+
+    private void flipPage(int address){
+        mPageView.abortAnimation();
+        MotionEvent e;
+        if(address<bookPageFactory.getReadAddress()){
+            //向前翻
+            mPageView.calcCornerXY(10, screenHeight-10);
+            e = MotionEvent.obtain(0, 0, MotionEvent.ACTION_MOVE, 20, screenHeight-20, 1);
+        }else {
+            //向后翻
+            mPageView.calcCornerXY(screenWidth-10, screenHeight-10);
+            e = MotionEvent.obtain(0, 0, MotionEvent.ACTION_MOVE, screenWidth-20, screenHeight-20, 1);
+        }
+        bookPageFactory.setReadAddress(address);
+        bookPageFactory.onDraw(mNextPageCanvas);
+        mPageView.setBitmaps(mCurPageBitmap, mNextPageBitmap);
+        mPageView.doTouchEvent(e);
+    }
+
+    int address=0;
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        Log.i(TAG,"onProgressChanged progress"+progress);
+        address=(bookPageFactory.getBookLength()*progress/100)-1;//-1是为了取得-1，-1是封面位置
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        Log.i(TAG,"onStopTrackingTouch");
+        flipPage(address);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(control.getVisibility()==View.VISIBLE){
+            hideSystemUI();
+            control.setVisibility(View.GONE);
+        }else {
+            finish();
+        }
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        hideSystemUI();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        hideSystemUI();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mCursor!=null){
+            setReadAddress(mCursor.getString(ArticleLoader.Query.TITLE),bookPageFactory.getReadAddress());
+        }
+    }
+}
